f7c4c6af63182e029ca7e87d21f51b3fbe9a945d
琦 陈 <448764699@qq.com>
2021-05-27 21:20:43 +0800

    socket.io

---
+++ mylearn/proj/socketio/socketio.js
@@ -0,0 +1,9 @@
+
+
+var server = require('http').createServer();
+var io = require('socket.io')(server);
+io.on('connection', function(client){
+  client.on('event', function(data){console.log(data)});
+  client.on('disconnect', function(){});
+});
+server.listen(3000);
