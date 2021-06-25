3ca3bddd1b08dadb6a9428ef375d0d43dbfb17fb
chenqi <chenqi1@sensetime.com>
2021-06-25 15:42:41 +0800

    api.ts

---
+++ mylearn/proj/koa/src/controllers/api.ts
@@ -0,0 +1,27 @@
+// 使用参考: https://www.liaoxuefeng.com/wiki/1022910821149312/1105003357927328
+var products = [{
+    name: 'iPhone',
+    price: 6999
+}, {
+    name: 'Kindle',
+    price: 999
+}];
+
+module.exports = {
+    'GET /api/products': async (ctx, next) => {
+        ctx.response.type = 'application/json';
+        ctx.response.body = {
+            products: products
+        };
+    },
+
+    'POST /api/products': async (ctx, next) => {
+        var p = {
+            name: ctx.request.body.name,
+            price: ctx.request.body.price
+        };
+        products.push(p);
+        ctx.response.type = 'application/json';
+        ctx.response.body = p;
+    }
+};
