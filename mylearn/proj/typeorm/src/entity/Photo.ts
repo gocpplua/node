342cd42399027845211bc403f4c802092158cf37
chenqi <chenqi1@sensetime.com>
2021-07-08 17:38:45 +0800

    typeorm

---
+++ mylearn/proj/typeorm/src/entity/Photo.ts
@@ -0,0 +1,26 @@
+import { Entity, Column, PrimaryGeneratedColumn, OneToOne } from "typeorm";
+import { PhotoMetadata } from "./PhotoMetadata";
+
+@Entity()
+export class Photo {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column("text")
+  description: string;
+
+  @Column()
+  filename: string;
+
+  @Column()
+  views: number;
+
+  @Column()
+  isPublished: boolean;
+
+  @OneToOne(type => PhotoMetadata, photoMetadata => photoMetadata.photo)
+  metadata: PhotoMetadata;
+}
