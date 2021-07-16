2d3e48a35cef4e78a04054767018d17c4be50503
琦 陈 <448764699@qq.com>
2021-07-16 09:47:41 +0800

    // GPSLongitude  和 GPSLatitude 获得的是度分秒，需要转换下             // 经纬度转换-----度分秒以及经纬度和米:https://cloud.tencent.com/developer/article/1046281             // 网页转换:https://www.opengps.cn/map/tools/gpsconvert.aspx             // 转换会就可以在地图上标记点:https://api.map.baidu.com/lbsapi/getpoint/index.html

--- mylearn/minitest/picposition.ts
+++ mylearn/minitest/picposition.ts
@@ -2,11 +2,15 @@
 var ExifImage = require('exif').ExifImage;
  
 try {
-    new ExifImage({ image : '/home/XX/XX/Desktop/d.jpeg' }, function (error, exifData) {
+    new ExifImage({ image : 'C:/Users/Administrator/Desktop/k.jpeg' }, function (error, exifData) {
         if (error)
             console.log('Error: '+error.message);
         else
             console.log(exifData); // Do something with your data!
+            // GPSLongitude  和 GPSLatitude 获得的是度分秒，需要转换下 
+            // 经纬度转换-----度分秒以及经纬度和米:https://cloud.tencent.com/developer/article/1046281
+            // 网页转换:https://www.opengps.cn/map/tools/gpsconvert.aspx
+            // 转换会就可以在地图上标记点:https://api.map.baidu.com/lbsapi/getpoint/index.html
     });
 } catch (error) {
     console.log('Error: ' + error.message);
