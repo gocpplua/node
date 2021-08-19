dba6b26b884cd8a84fb4416d14c8d3e7b775e25a
chenqi <chenqi1@sensetime.com>
2021-08-19 21:08:17 +0800

    改变目录结构

--- mylearn/proj/behavior3_behavior3js/Demo1/index.ts
+++ mylearn/proj/behavior3_behavior3js/Demo1/index.ts
@@ -1,17 +1,17 @@
 var fs=require('fs')
 var b3 = require('behavior3js')
-fs.readFile('demo.json','utf8',function (err: any, data: any) {
+
+console.log(b3.VERSION)
+
+fs.readFile(__dirname + '/demo.json','utf8',function (err: any, data: any) {
   if(err) console.log(err);
   var test1=JSON.parse(data);//读取的值
-  console.log('-----------json----------------')
-  console.log(test1)
 
-  var tree = new b3.BehaviorTree();
-
-  
+  var tree = new b3.BehaviorTree(); 
   tree.load(test1)
   console.log('-----------tree----------------')
   console.log(tree.dump())
+  
   console.log('-----------tick----------------')
   var blackboard = new b3.Blackboard();
   blackboard.set('var1', 'value', 'tree1');
